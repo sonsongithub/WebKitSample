bb7d41e51f6500fc5b4f43bb68eab157b7ea4b53
Yuichi Yoshida <yoshida.yuichi@gmail.com>
2014-06-05 09:02:36 +0900

    Added delegate methods.

--- WebKit/ViewController.m
+++ WebKit/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "helper.h"
+
 #import <WebKit/WebKit.h>
 
-@interface ViewController ()
+@interface ViewController () <WKNavigationDelegate, WKUIDelegate>
             
 
 @end
@@ -24,22 +26,74 @@
 	webView.translatesAutoresizingMaskIntoConstraints = NO;
 	[self.view addSubview:webView];
 	
+	webView.navigationDelegate = self;
+	webView.UIDelegate = self;
+	
 	NSDictionary *views = NSDictionaryOfVariableBindings(webView);
 	
 	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-0-[webView(>=0)]-0-|"
 																	  options:0
 																	  metrics:nil
 																		views:views]];
-	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-0-[webView(>=0)]-0-|"
+	[self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-20-[webView(>=0)]-0-|"
 																	  options:0
 																	  metrics:nil
 																		views:views]];
 	[webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
 }
 
-- (void)didReceiveMemoryWarning {
-	[super didReceiveMemoryWarning];
-	// Dispose of any resources that can be recreated.
+#pragma mark - WKNavigationDelegate
+
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView didFailNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation withError:(NSError *)error {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation {
+	DNSLogMethod
+	DNSLog(@"%@", navigation);
+}
+
+//- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+//}
+//
+//- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler {
+//	
+//}
+
+#pragma mark - WKUIDelegate
+
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures {
+	DNSLogMethod
+	return webView;
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)())completionHandler {
+	DNSLogMethod
+	
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler {
+	DNSLogMethod
+}
+
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString *result))completionHandler {
+	DNSLogMethod
 }
 
 @end
